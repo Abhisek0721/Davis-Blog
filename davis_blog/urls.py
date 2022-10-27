06f8a18d70262f3b11e8828b79c9fd7b99ac01a1
dsavis <davislouis565@gmail.com>
2022-10-27 11:29:40 +0530

    davis blog

---
+++ davis_blog/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from blogs import views
+
+admin.site.site_header = "Davis Blog"
+admin.site.site_title =  "Davis Blog"
+admin.site.index_title = "Davis Blog"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("blog", views.blog, name="blog"),
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+]
+
