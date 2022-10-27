7cd0b025e022634639a6f1e799a4593a490eeb94
Abhisek Upadhaya <Abhisek0721@gmail.com>
2022-10-27 22:53:21 +0530

    signup added for backend

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,16 @@
+import email
+from enum import unique
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class Account(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField()
+    password = models.JSONField() # JSONField because it will be stored in encripted form
+    datetime = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.username
--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.signing import Signer #for encrypting the password
+import json
+from .models import Account
+
+signer = Signer(salt='extra') # for encription of password
 
 # Create your views here.
 def home(request):
@@ -14,8 +19,38 @@ def blog(request):
     return render(request, "blog.html")
 
 def login(request):
+    if (request.method == "POST"):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        check_user = Account.objects.filter(username=username).first()
+        if (check_user):
+            jsonPassword = json.dumps(signer.unsign_object(check_user.password)) #dumps() converts python dictionary into json
+            dictPassword = json.loads(jsonPassword) #loads() converts json into python dictionary
+            if(password==dictPassword['password']):
+                # Login Successful
+                return redirect('/')
+            else:
+                # Wrong Password
+                return redirect('/login')
+        else:
+            # username doesn't exist
+            return redirect('/login')
     return render(request, "login.html")
 
 def signup(request):
+    if (request.method == "POST"):
+        firstName = request.POST.get('fname')
+        lastName = request.POST.get('lname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        check_user = Account.objects.filter(username=username).exists()
+        if(check_user):
+            return redirect('/signup')
+        else:
+            password = signer.sign_object({'password': password}) #encrypted password
+            User = Account.objects.create(firstName=firstName, lastName=lastName, username=username, email=email, password=password)
+            User.save()
+            return redirect('/login')
     return render(request, "signup.html")
 
