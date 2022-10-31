e63710363e12436e963e24ac29d96f14d618ed88
Abhisek Upadhaya <Abhisek0721@gmail.com>
2022-11-01 00:35:00 +0530

    used buildin authenticate login and logout

--- blogs/models.py
+++ blogs/models.py
@@ -1,25 +1,12 @@
-import email
-from enum import unique
 from django.db import models
 from django.utils.timezone import now
-
-# Create your models here.
-class Account(models.Model):
-    firstName = models.CharField(max_length=30)
-    lastName = models.CharField(max_length=30)
-    username = models.CharField(max_length=30, unique=True)
-    email = models.EmailField()
-    password = models.JSONField() # JSONField because it will be stored in encripted form
-    datetime = models.DateTimeField(default=now)
-
-    def __str__(self):
-        return self.username
 
 class Contact(models.Model):
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
     email = models.EmailField()
     message = models.TextField()
+    datetime = models.DateTimeField(default=now)
 
     def __str__(self):
         return self.email
--- blogs/views.py
+++ blogs/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render, redirect
-from django.core.signing import Signer #for encrypting the password
-import json
-from .models import Account, Contact
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from .models import Contact
 
-
-signer = Signer(salt='extra') # for encription of password
 
 # Create your views here.
 def home(request):
-    if ('auth' in request.session):
-        data = {
-                "auth" : request.session["auth"],
-                "username": request.session["username"], 
-                "name" : request.session["fname"]+" "+request.session["lname"],
-                "email" : request.session["email"]
-            }
-        return render(request, 'index.html', data)
-    else:
-        return render(request, 'index.html')
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -40,23 +29,12 @@ def login(request):
     if (request.method == "POST"):
         username = request.POST.get('username')
         password = request.POST.get('password')
-        check_user = Account.objects.filter(username=username).first()
-        if (check_user):
-            jsonPassword = json.dumps(signer.unsign_object(check_user.password)) #dumps() converts python dictionary into json
-            dictPassword = json.loads(jsonPassword) #loads() converts json into python dictionary
-            if(password==dictPassword['password']):
-                # Login Successful
-                request.session['auth'] = True
-                request.session['username'] = username
-                request.session["fname"] = check_user.firstName
-                request.session['lname'] = check_user.lastName
-                request.session["email"] = check_user.email
-                return redirect('/')
-            else:
-                # Wrong Password
-                return redirect('/login')
+        check_user = authenticate(username=username, password=password)
+        if (check_user is not None):
+            auth_login(request, check_user)
+            return redirect('/')
         else:
-            # username doesn't exist
+            # wrong username or password
             return redirect('/login')
     return render(request, "login.html")
 
@@ -67,23 +45,18 @@ def signup(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        check_user = Account.objects.filter(username=username).exists()
+        check_user = User.objects.filter(username=username).first()
         if(check_user):
             return redirect('/signup')
         else:
-            password = signer.sign_object({'password': password}) #encrypted password
-            User = Account.objects.create(firstName=firstName, lastName=lastName, username=username, email=email, password=password)
-            User.save()
+            create_account = User.objects.create_user(username, email, password)
+            create_account.first_name = firstName
+            create_account.last_name = lastName
+            create_account.save()
             return redirect('/login')
     return render(request, "signup.html")
 
 def logout(request):
-    request.session.pop('auth')
-    request.session.pop('username')
-    request.session.pop("fname")
-    request.session.pop('lname')
-    request.session.pop("email")
+    auth_logout(request)
     return redirect('/login')
-
-
 
